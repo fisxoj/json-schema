6be41edceb4b4194d0ee292bc970d779dc195fbd
Matt Novenstern <fisxoj@gmail.com>
2020-05-01 10:31:22 -0400

    patternProperties and additionalProperties with type info

--- src/validators.lisp
+++ src/validators.lisp
@@ -24,46 +24,94 @@
 
   (ecase schema-version
     (:draft2019-09
-     (let ((resolved-schema (reference:ensure-resolved schema)))
+     (let* ((resolved-schema (reference:ensure-resolved schema))
+            (results nil))
 
-       (notany #'null
-               (loop for (property . value) in (json::jso-alist resolved-schema)
-                     for result = (handler-case (draft2019-09 resolved-schema property value data)
-                                    (no-validator-condition (c)
-                                      (warn "No validator for field ~a - skipping."
-                                            (slot-value c 'field-name))
-                                      c))
+       (json:mapjso (lambda (property value)
+                      (let ((result (handler-case (draft2019-09 resolved-schema
+                                                                property
+                                                                value
+                                                                data)
 
-                     ;; do (format t
-                     ;;            "~& validate ~a~%  property ~S~%  value ~a~%  data ~a~%  => ~a~%"
-                     ;;            resolved-schema
-                     ;;            property
-                     ;;            value
-                     ;;            data
-                     ;;            result)
-                     ;; do (format t "~& validate property ~S => ~a~%" property result)
-                     unless (typep result 'condition)
-                       collecting result))))))
+                                      (no-validator-condition (c)
+                                        (warn "No validator for field ~a - skipping."
+                                              (slot-value c 'field-name))
+                                        c))))
+
+                        ;; (format t "~& validate property ~S => ~A~%" property result)
+
+                        (unless (typep result 'condition)
+                          (push result results))))
+                    resolved-schema)
+
+       ;; (format t "~& validating data ~a against ~a => ~a~%"
+       ;;         data
+       ;;         resolved-schema
+       ;;         (notany #'null results))
+
+       (notany #'null results)))))
 
 
 (defun additional-properties (schema value data)
   (unless (typep data 'st-json:jso)
     (return-from additional-properties t))
 
-  (if (eq value :false)
-      (let ((schema-properties (mapcar #'car (st-json::jso-alist (json:getjso "properties" schema))))
-            (data-properties (mapcar #'car (st-json::jso-alist data))))
+  (labels ((alist-keys (alist)
+             (mapcar #'car (st-json::jso-alist alist)))
 
-        #+nil(format t "~& data-properties: ~a~% schema-properties: ~a~%different-properties: ~a~%"
-                     data-properties
-                     schema-properties
-                     (set-difference schema-properties data-properties :test #'string=))
+           (remove-pattern-property-keys (list)
+             (if-let ((pattern-properties (json:getjso "patternProperties" schema)))
 
-        (null
-         (set-difference schema-properties
-                         data-properties
-                         :test #'string=)))
-      t))
+               (remove-if-not (lambda (key)
+                                (some (lambda (pattern) (pattern schema pattern key))
+                                      (alist-keys pattern-properties)))
+                              list)
+               list)))
+
+    (cond
+      ((eq value :false)
+       (let* ((schema-properties (when-let ((properties (json:getjso "properties" schema)))
+                                   (alist-keys properties)))
+
+              (data-properties (alist-keys data))
+
+              ;; pattern properties don't count as additional
+              (additional-properties (remove-pattern-property-keys
+                                      (set-difference data-properties
+                                                      schema-properties
+                                                      :test #'string=)))
+
+              (combined-schema-properties (union schema-properties
+                                                 additional-properties
+                                                 :test #'string=)))
+
+         ;; (format t "~& data-properties: ~a~% schema-properties: ~a~%different-properties: ~a~%"
+         ;;         data-properties
+         ;;         combined-schema-properties
+         ;;         (set-difference combined-schema-properties data-properties :test #'string=))
+
+         (null
+          (set-difference data-properties
+                          combined-schema-properties
+                          :test #'string=))))
+
+      ((typep value 'json:jso)
+       (let* ((schema-properties (when-let ((properties (json:getjso "properties" schema)))
+                                   (alist-keys properties)))
+
+              (data-properties (alist-keys data))
+
+              ;; pattern properties don't count as additional
+              (additional-properties (remove-pattern-property-keys
+                                      (set-difference data-properties
+                                                      schema-properties
+                                                      :test #'string=))))
+         (notany #'null
+                 (loop for additional-property in additional-properties
+                       collect (validate value (json:getjso additional-property data))))))
+
+      (t
+       t))))
 
 
 (defun validate-type (schema type data)
@@ -95,6 +143,15 @@
                                result)))))
 
 
+(defun pattern (schema pattern data)
+  (declare (ignore schema))
+
+  (unless (typep data 'string)
+    (return-from pattern t))
+
+  (ppcre:scan pattern data))
+
+
 (defmacro def-validator (name &rest keys-plist)
   `(defun ,name (schema field value data)
      (alexandria:switch (field :test #'string-equal)
@@ -106,4 +163,5 @@
 (def-validator draft2019-09
   "additionalProperties" additional-properties
   "type" validate-type
-  "properties" properties)
+  "properties" properties
+  "pattern" pattern)
