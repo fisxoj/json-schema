31de324f2f82566c4ce1078324dd32e60d197222
Matt Novenstern <fisxoj@gmail.com>
2020-10-25 02:40:57 -0400

    Update documentation for new signature of validate function

--- src/json-schema.lisp
+++ src/json-schema.lisp
@@ -15,7 +15,7 @@
 
 
 (defun validate (data &key (schema-version *schema-version*) (pretty-errors-p t) schema)
-  "The primary validation function for json-schema.  Takes a schema of type hash-table, data: which can be a simple value or an object as a hash table, and then optionally accepts a schema version and pretty-errors-p deterimines whether the second return value is exception objects or strings of the rendered errors (strings by default)."
+  "The primary validation function for json-schema.  Takes data: which can be a simple value or an object as a hash table, and then optionally accepts a schema (if the data doesn't contain a top-level ``$schema`` key), schema version and pretty-errors-p deterimines whether the second return value is exception objects or strings of the rendered errors (strings by default)."
 
   (let ((schema (or schema (json-schema.parse:parse (dex:get (json-schema.utils:object-get "$schema" data) :force-string t)))))
     (reference:with-context ((reference:get-id-fun-for-draft schema-version))
