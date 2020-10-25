a26a517f996829e59c0d8bd7dd30b7d59f43d20c
Matt Novenstern <fisxoj@gmail.com>
2020-10-25 02:33:08 -0400

    json-schema: Change validate function signature
    Take the data to be validated as the only required parameter.  A
    schema can be optionally supplied using the :schema parameter.  This
    breaks the API hard.

--- src/json-schema.lisp
+++ src/json-schema.lisp
@@ -14,11 +14,12 @@
 (defparameter *schema-version* :draft7)
 
 
-(defun validate (schema data &key (schema-version *schema-version*) (pretty-errors-p t))
-  "The primary validation function for json-schema.  Takes a schema of type hash-table, data: which can be a simple value or an object as a hash table, and then optionall accepts a schema version and pretty-errors-p deterimines whether the second return value is exception objects or strings of the rendered errors (strings by default)."
+(defun validate (data &key (schema-version *schema-version*) (pretty-errors-p t) schema)
+  "The primary validation function for json-schema.  Takes a schema of type hash-table, data: which can be a simple value or an object as a hash table, and then optionally accepts a schema version and pretty-errors-p deterimines whether the second return value is exception objects or strings of the rendered errors (strings by default)."
 
-  (reference:with-context ((reference:get-id-fun-for-draft schema-version))
-    (reference:with-pushed-context (schema)
-      (if-let ((errors (validators:validate schema data schema-version)))
-        (values nil (mapcar (if pretty-errors-p #'princ-to-string #'identity) errors))
-        (values t nil)))))
+  (let ((schema (or schema (json-schema.parse:parse (dex:get (json-schema.utils:object-get "$schema" data) :force-string t)))))
+    (reference:with-context ((reference:get-id-fun-for-draft schema-version))
+      (reference:with-pushed-context (schema)
+        (if-let ((errors (validators:validate schema data schema-version)))
+          (values nil (mapcar (if pretty-errors-p #'princ-to-string #'identity) errors))
+          (values t nil))))))
